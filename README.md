016bd21fb51bffedb5ee0312fae68509e5cb7a82
Avinash Kumar <nashy18@gmail.com>
2018-04-21 19:50:03 +0530

    User Module updated

--- README.md
+++ README.md
@@ -28,3 +28,7 @@ Before running the tests make sure you are serving the app via `ng serve`.
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+##Debugging Angular into Visual Studio Code
+https://www.c-sharpcorner.com/article/debugging-angular-using-visual-studio-code-vscode/
+Note: launch.json file already added within the repository
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,17 +13,53 @@ import 'rxjs/add/operator/map';
   providers: [UserService]
 })
 export class UserComponent implements OnInit {
-  itemCode = [
-    "Item-01", "Item-02", "Item-03", "Item-04"
+  productionLine = [
+    { "name": "Production-1", "code": "P1" },
+    { "name": "Production-2", "code": "P2" },
+    { "name": "Production-3", "code": "P3" },
+    { "name": "Production-4", "code": "P4" }
+  ]
+  netWeight = [
+    "10", "20", "30", "40"
   ];
+  // brandName = [
+  //   "Brand-01", "Brand-02", "Brand-03", "Brand-04"
+  // ];
+  brandNameData = [
+    { "name": "Brand-01", "code": "B1", "productionLineCode": "P1" },
+    { "name": "Brand-02", "code": "B2", "productionLineCode": "P1" },
+    { "name": "Brand-03", "code": "B3", "productionLineCode": "P2" },
+    { "name": "Brand-04", "code": "B4", "productionLineCode": "P4" }
+  ];
+  productName = [
+    "Product-01", "Product-02", "Product-03", "Product-04"
+  ]
   public formData: Object;
-
+  public brandName: Array<object>;
   constructor(private http: HttpClient) {
-    this.formData={};
+    this.formData = {};
+    //this.formData["productionLine"] = "P2";
+    //this.brandName = this.brandNameData;
+  }
+
+  onSubmit(event, data) {
+    console.log(data);
   }
 
   ngOnInit() {
 
+  }
+
+  productionLineSelected(data) {
+    let result = [];
+    delete this.formData["brandName"];//for deleting the pre selected value of brand name dropdown
+    //creating filter of data based on production line selection, populate btand name dropdown
+    this.brandNameData.forEach(item => {
+      if (item.productionLineCode == data.code) {
+        result.push(item);
+      }
+    });
+    this.brandName = result;
   }
 }
 
